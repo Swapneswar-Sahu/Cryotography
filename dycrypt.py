e4e7b9e815acd42d4b2e56fb7ebb0d3aefa8821f
swapneswarsahu150@gmail.com <swapneswarsahu150@gmail.com>
2022-11-20 19:29:55 +0530

    final

---
+++ dycrypt.py
@@ -0,0 +1,18 @@
+def decrypt(content, key):
+    result = ''
+    key = key % 10
+    for letter in content:
+        num = ord(letter)
+        num = num - key
+        result += chr(num)
+    return result
+
+fname = input('Enter the file name: ')
+key = int(input('Enter the key: '))
+fp = open(fname, 'r')
+content = fp.read()
+fp.close()
+new_content = decrypt(content, key)
+fp = open(fname, 'w')
+fp.write(new_content)
+fp.close()
